8ef5329d1502e9cbaa71f4374ef6e12391db4650
matheusDall <matheus.dalmasomariano@gmail.com>
2024-10-25 12:00:45 -0300

    criacao da tela de titulo

---
+++ studyandtea/app/src/main/java/com/example/studyandtea/Telatempoactivity.java
@@ -0,0 +1,14 @@
+package com.example.studyandtea;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Telatempoactivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_telatempoactivity);
+    }
+}
